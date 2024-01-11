0888ab331a2e3279fba04cd907626847701af6cb
LimpanGH <Linusj555@gmail.com>
2024-01-11 12:08:19 +0100

    jic

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Codealong
 Kör filen genom att skriva node <filens namn> i terminalen
+
+Vite:
+
+- Skapar en mapp med filer från vite: npm create vite@latest
+- Installerar vite: npm install
+- kör vite-biblioteket: npm run dev (kan även bara skriva vite)
+- Installera tex axios: npm install axios
+- Tryck på länken ist för att öppna i live server ➜ Local: http://localhost:5174/
--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello Node from JS');
+// console.log('Hello Node from JS');
+
+// Fetcha med axios i node -------------------------------
+const axios = require('axios') // Importerar
+const url = "https://jsonplaceholder.typicode.com/users"
+async function getUser(url) {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getUser(url)
